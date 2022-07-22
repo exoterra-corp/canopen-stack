d9c412d92d67e40df0c302a83a05a82c33b65711
Jeremy Mitchell <jmitchell@exoterracorp.com>
2022-07-22 12:32:56 -0600

    JTM - added sonarqube config

--- canopen-stack.X/nbproject/Makefile-variables.mk
+++ canopen-stack.X/nbproject/Makefile-variables.mk
@@ -11,3 +11,10 @@ CND_ARTIFACT_PATH_default=dist/default/production/canopen-stack.X.a
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
 CND_PACKAGE_NAME_default=canopen-stack.X.tar
 CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/canopen-stack.X.tar
+# sonarqube configuration
+CND_ARTIFACT_DIR_sonarqube=dist/sonarqube/production
+CND_ARTIFACT_NAME_sonarqube=canopen-stack.X.a
+CND_ARTIFACT_PATH_sonarqube=dist/sonarqube/production/canopen-stack.X.a
+CND_PACKAGE_DIR_sonarqube=${CND_DISTDIR}/sonarqube/package
+CND_PACKAGE_NAME_sonarqube=canopen-stack.X.tar
+CND_PACKAGE_PATH_sonarqube=${CND_DISTDIR}/sonarqube/package/canopen-stack.X.tar
